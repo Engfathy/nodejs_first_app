ff33781c43f63d57ea680ec4e2289d7d6ccc091a
Eng=fathy <87507862+Engfathy@users.noreply.github.com>
2023-07-24 18:14:05 +0300

    Add files via upload

---
+++ first-app/index.js
@@ -0,0 +1,24 @@
+const Http = require("node:http");
+
+
+    const server = Http.createServer((req, res) => {
+        if (req.url == "/") {
+            res.writeHead(200, {
+                "content-Type": "text",
+                fromMe: "fuck you",
+            });
+            res.end("fast page");
+        } else if (req.url == "/about") {
+            res.writeHead(200, {
+                "content-Type": "text/plain",
+                fromMe: "fuck you",
+            });
+            res.end("about");
+        }
+    });
+
+    const PORT =8000 || process.env.PORT ;
+
+    server.listen(PORT, () => {
+        console.log(`Server is running at port ${PORT}`);
+    });
